12da81f437c697849f988d19c4327247f811566f
hyenny <qlwb9191@naver.com>
2021-08-24 11:42:32 +0900

    Comparator 설명 추가

--- src/com/algorithm/search/KClosetPoints.java
+++ src/com/algorithm/search/KClosetPoints.java
@@ -13,6 +13,9 @@ public class KClosetPoints {
 
     public int[][] solve_example(int[][] points, int k) {
         int [][] result = new int[k][2];
+        // x, y 평면에서 (x1, y1), (x2, y2) 두 점 사이 거리 구하는 공식
+        // (x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)
+        // 한 점은 무조건 원점(x1 = 0, y1 = 0)이므로 (x1 * x1) + (y1 * y1)
         Queue<int[]> pq = new PriorityQueue<>((a,b) -> (a[0]*a[0] + a[1]*a[1]) - (b[0]*b[0] + b[1]*b[1]));
 
         for (int[] p : points) {
