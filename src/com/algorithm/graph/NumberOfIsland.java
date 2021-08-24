5bf7579af05c092026482d824507598c909e04e6
hyenny <qlwb9191@naver.com>
2021-08-24 11:12:08 +0900

    섬의 수 bfs 문제 풀이

--- src/com/algorithm/graph/NumberOfIsland.java
+++ src/com/algorithm/graph/NumberOfIsland.java
@@ -1,5 +1,8 @@
 package com.algorithm.graph;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class NumberOfIsland {
     public static void main(String[] args) {
         int[][] grid = {
@@ -56,6 +59,42 @@ public class NumberOfIsland {
         for (int[] dir : DIRS) {
             //System.out.println(Arrays.toString(dir));
             dfs(grid, i + dir[0], j + dir[1]);
+        }
+    }
+
+    public int solve_bfs(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0)
+            return 0;
+
+        m = grid.length;
+        n = grid[0].length;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+                    cnt++;
+                    bfs(grid, i, j);
+                }
+            }
+        }
+        return cnt;
+    }
+
+    private void bfs(int[][] grid, int i, int j) {
+        grid[i][j] = -1;
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{i,j});
+        while(!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            for(int[] dir : DIRS) {
+                int x1 = cur[0] + dir[0];
+                int y1 = cur[1] + dir[1];
+
+                if (x1 >=0 && y1 >= 0 && x1 < m && y1 < n && grid[x1][y1] == 1) {
+                    grid[x1][y1] = -1;
+                    queue.offer(new int[]{x1,y1});
+                }
+            }
         }
     }
 
